4d088e75d8ab4f111432027a3ec15ee1c3a59ef5
Morgan Lizop <morgan.lizop@iese.net>
2020-05-14 16:50:37 +0100

    Cocktail cards now display on front page

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,34 @@
+class DosesController < ApplicationController
+def index
+    @doses = Dose.all
+  end
+
+  def show
+    @dose = Dose.find(params[:id])
+  end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(params[dose_params])
+    @dose.save
+  end
+
+  def edit
+    @dose = Dose.find(params[:id])
+  end
+
+  def update
+    @dose = Dose.find(params[:id])
+    @dose.update(params[dose_params])
+    redirect_to cocktail_path(@dose)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient, :cocktail)
+  end
+end
