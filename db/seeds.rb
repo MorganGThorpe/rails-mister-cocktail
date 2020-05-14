5efaa7194cd428143e589fb3648b2863b1c5e047
Morgan Lizop <morgan.lizop@iese.net>
2020-05-14 15:00:30 +0100

    validations and associations all working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "rhum")
+Ingredient.create(name: "whisky")
+Ingredient.create(name: "Elderflower cordial")
+Ingredient.create(name: "apple juice")
+Ingredient.create(name: "blood orange juice")
+Ingredient.create(name: "basil leaves")
+Ingredient.create(name: "pear juice")
+Ingredient.create(name: "strawberry")
+Ingredient.create(name: "cointreau")
