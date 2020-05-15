1d649d53f0f363d57262d9b3bd98509ee6ff2cc1
Morgan Lizop <morgan.lizop@iese.net>
2020-05-15 13:00:38 +0100

    User can now see a list of cocktails and each cocktail's details

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,88 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Cocktail.destroy_all
-
-Cocktail.create(name: "Brutalizer")
-Cocktail.create(name: "Pink Poppies")
-Cocktail.create(name: "Fraulein Gardens")
-Cocktail.create(name: "Loca Loca")
-Cocktail.create(name: "Brain Feather")
-Cocktail.create(name: "Chestnut Shortcut")
-Cocktail.create(name: "Poor Choice/Good Evening")
-
-byebug
-
 Ingredient.destroy_all
 
-Ingredient.create(name: "Rhum")
-Ingredient.create(name: "Honey")
-Ingredient.create(name: "Edelweiss Flower")
-Ingredient.create(name: "Beer")
-Ingredient.create(name: "Blood Orange")
-Ingredient.create(name: "Creme de Maron")
-Ingredient.create(name: "Pastis")
 
-Dose.destroy_all
+cocktail_titles = [
+  "Brutalizer",
+  "Pink Poppies",
+  "Fraulein Gardens",
+  "Loca Loca",
+  "Brain Feather",
+  "Chesnut Shortcut",
+  "Poor Choice / Good Evening",
+  "Pan Galactic Garggle Blaster"
+]
+ingredient_names = [
+  "Rhum",
+  "Honey",
+  "Edleweiss Flower",
+  "Beer",
+  "Blood Orange",
+  "Creme de marron",
+  "Pastis"
+]
+dose_descriptions = [
+  "A little bit",
+  "Juste un doigt",
+  "3 cl",
+  "A splash"
+]
 
-Dose.create(description: "A little bit", cocktail: Cocktail.all, ingredient: Ingredient.all.sample)
-Dose.create(description: "A little bit more", cocktail: Cocktail.all, ingredient: Ingredient.all.sample)
+ingredient_names.each do |name|
+  Ingredient.create(name: name)
+end
+
+cocktail_titles.each do |title|
+  cocktail = Cocktail.new
+  cocktail.name = title
+  cocktail.save
+  dose_descriptions.each do |description|
+    dose = Dose.new
+    dose.description = description
+    dose.cocktail = cocktail
+    dose.ingredient = Ingredient.all.sample
+    dose.save
+  end
+end
+
+# dose_descriptions.each do |description|
+#   dose = Dose.new
+#   dose.description = description
+#   dose.cocktail = [Cocktail.all.sample]
+#   dose.ingredient = Ingredient.all.sample
+#   dose.save
+#   p dose.errors
+# end
+
+# Cocktail.destroy_all
+
+# Cocktail.create(name: "Brutalizer")
+# jus_de_froot_loops = Cocktail.new(name: "jus de froot loops")
+# jus_de_froot_loops.save
+# Cocktail.create(name: "Pink Poppies")
+# Cocktail.create(name: "Fraulein Gardens")
+# Cocktail.create(name: "Loca Loca")
+# Cocktail.create(name: "Brain Feather")
+# Cocktail.create(name: "Chestnut Shortcut")
+# Cocktail.create(name: "Poor Choice/Good Evening")
+
+# Ingredient.destroy_all
+# fruit = Ingredient.new(name: "fruit")
+# fruit.save
+# Ingredient.create(name: "Rhum")
+# Ingredient.create(name: "Honey")
+# Ingredient.create(name: "Edelweiss Flower")
+# Ingredient.create(name: "Beer")
+# Ingredient.create(name: "Blood Orange")
+# Ingredient.create(name: "Creme de Maron")
+# Ingredient.create(name: "Pastis")
+
+# Dose.destroy_all
+# byebug
+# nouvel_dose = Dose.new(description: "A little bit")
+# nouvel_dose.cocktail = [jus_de_froot_loops]
+# nouvel_dose.ingredient = fruit
+# nouvel_dose.save
+# Dose.create(description: "A little bit more", cocktail: Cocktail.all, ingredient: Ingredient.all.sample)
